52421ea1b8e2e4cd67472f746614653a7b9b03ad
Steffen Schroeder <st.schroeder@gmail.com>
2020-01-08 20:14:45 +0100

    added error handling

--- download_images.py
+++ download_images.py
@@ -14,8 +14,16 @@ def main():
 
 def download_image_to_disk(image_url):
     response = requests.get(image_url)
-    with open("result.jpeg", "wb") as result_file:
-        result_file.write(response.content)
+    if response.ok:
+        with open("result.jpeg", "wb") as result_file:
+            result_file.write(response.content)
+    elif response.status_code == 404:
+        print(f"Image {image_url} does not exist", file=sys.stderr)
+    else:
+        print(
+            f"Image {image_url} cannot be downloaded (HTTP Status: {response.status_code})",
+            file=sys.stderr,
+        )
 
 
 if __name__ == "__main__":
--- test_download_images.py
+++ test_download_images.py
@@ -13,3 +13,10 @@ def test_download_single_image(target_dir):
     download_image_to_disk("https://httpbin.org/image/jpeg")
     expected_file = target_dir.join("result.jpeg")
     assert expected_file.exists()
+
+
+@pytest.mark.parametrize("status_code", [404, 500])
+def test_download_but_failing(target_dir, status_code):
+    download_image_to_disk(f"https://httpbin.org/status/{status_code}")
+
+    assert len(target_dir.listdir()) == 0
