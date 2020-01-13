2c355d05b68075467a226226dcef49c6e2c96afa
Steffen Schroeder <st.schroeder@gmail.com>
2020-01-13 11:24:53 +0100

    refactored tests

--- test_download_images.py
+++ test_download_images.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 
 from download_images import _get_filename_by_url
@@ -5,29 +7,21 @@ from download_images import download_image_to_disk
 from download_images import download_images_from_files
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def target_dir(tmpdir):
     tmpdir.chdir()
     yield tmpdir
 
 
-@pytest.fixture
-def a_file(target_dir):
-    target_file_path = target_dir.join("file1.txt")
-    with open(target_file_path, "w") as f:
-        yield f
+def write_file_with_content(filename, *lines):
+    with open(filename, "w") as f:
+        for line in lines:
+            f.writelines(line + "\n")
 
 
-@pytest.fixture
-def b_file(target_dir):
-    target_file_path = target_dir.join("file2.txt")
-    with open(target_file_path, "w") as f:
-        yield f
-
-
-def test_download_single_image(target_dir):
+def test_download_single_image():
     download_image_to_disk("https://picsum.photos/200.jpg")
-    assert len(target_dir.listdir()) == 1
+    assert_current_folder_contains_files("200.jpg")
 
 
 @pytest.mark.parametrize("status_code", [404, 500])
@@ -41,20 +35,35 @@ def test_download_no_url(target_dir):
     assert len(target_dir.listdir()) == 0
 
 
-def test_download_from_file(target_dir, a_file):
-    print("https://picsum.photos/200.jpg", file=a_file, flush=True)
+def test_download_from_file():
+    write_file_with_content("file1.txt", "https://picsum.photos/200.jpg")
+
     download_images_from_files(["file1.txt"])
-    assert len(target_dir.listdir()) == 2
+
+    assert_current_folder_contains_files("file1.txt", "200.jpg")
 
 
-def test_download_from_multiple_files(target_dir, a_file, b_file):
-    print("https://picsum.photos/200.jpg", file=a_file, flush=True)
-    print("https://picsum.photos/300.jpg", file=a_file, flush=True)
-    print("https://picsum.photos/150.jpg", file=b_file, flush=True)
-    print("https://picsum.photos/50.jpg", file=b_file, flush=True)
+def test_download_from_multiple_files():
+    write_file_with_content(
+        "file1.txt", "https://picsum.photos/100.jpg", "https://picsum.photos/200.jpg"
+    )
+    write_file_with_content(
+        "file2.txt", "https://picsum.photos/300.jpg", "https://picsum.photos/400.jpg"
+    )
+
     download_images_from_files(["file1.txt", "file2.txt"])
-    assert len(target_dir.listdir()) == 6
+
+    assert_current_folder_contains_files(
+        "file1.txt", "file2.txt", "100.jpg", "200.jpg", "300.jpg", "400.jpg"
+    )
 
 
 def test_get_filename_by_url():
     assert _get_filename_by_url("https://picsum.photos/200.jpg") == "200.jpg"
+
+
+def assert_current_folder_contains_files(*filenames):
+    file_in_directory = [
+        filename.name for filename in Path().glob("*") if filename.is_file()
+    ]
+    assert set(file_in_directory) == set(filenames)
